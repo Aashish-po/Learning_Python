406bc4d503d9212c00936d54d6dc85443ad3cb80
Aashsish <poudelashish572@gmail.com>
2026-04-21 16:37:53 +0545

    Refactor password manager for improved key handling and code clarity

--- project/password_manager.py
+++ project/password_manager.py
@@ -1,29 +1,29 @@
 from cryptography.fernet import Fernet
 
-master_pwd = input("Enter your password: ")
-key = Fernet.generate_key()
-fernet = Fernet(key)
-
-
+"""
 def write_key():
+    key = Fernet.generate_key()
     with open("key.key", "wb") as key_file:
-        key_file.write(key)
+        key_file.write(key)"""
 
 
 def load_key():
-    return open("key.key", "rb").read()
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
 
 
-key = load_key() + master_pwd.encode()
-fernet = Fernet(key)
+key = load_key()
+fer = Fernet(key)
 
 
 def view():
-    with open("passwords.txt") as f:
+    with open("passwords.txt", "r") as f:
         for line in f.readlines():
             data = line.rstrip()
-            name, pwd = data.split("|")
-            print("Name:", name, "| Password:", fernet.decrypt(pwd.encode()).decode())
+            user, passw = data.split("|")
+            print("User:", user, "| Password:", fer.decrypt(passw.encode()).decode())
 
 
 def add():
@@ -31,11 +31,13 @@ def add():
     pwd = input("Password: ")
 
     with open("passwords.txt", "a") as f:
-        f.write(name + "|" + fernet.encrypt(pwd.encode()).decode() + "\n")
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
 
 
 while True:
-    mode = input("would you like to add password or view password(view, add)")
+    mode = input(
+        "Would you like to add a new password or view existing ones (view, add), press q to quit? "
+    ).lower()
     if mode == "q":
         break
 
@@ -44,5 +46,5 @@ while True:
     elif mode == "add":
         add()
     else:
-        print("invalid mode")
+        print("Invalid mode.")
         continue
